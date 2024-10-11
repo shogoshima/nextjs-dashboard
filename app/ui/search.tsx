4692ec7b9b671c877a81f52f3965631de9ded608
shogoshima <shogoshima@usp.br>
2024-10-11 10:01:12 -0300

    pagination and searching complete

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -10,15 +10,17 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
 
   const handleSearch = useDebouncedCallback((term) => {
-  console.log(`Searching... ${term}`);
- 
-  const params = new URLSearchParams(searchParams);
-  if (term) {
-    params.set('query', term);
-  } else {
-    params.delete('query');
-  }
-  replace(`${pathname}?${params.toString()}`);
+    console.log(`Searching... ${term}`);
+  
+    const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
+
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
   }, 300);
   
   return (
